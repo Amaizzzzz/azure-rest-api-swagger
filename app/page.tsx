4f73e844f7934e1c83e3d468ca4da30811155743
Zheyu Shen <zheyushen@microsoft.com>
2023-08-11 14:30:08 +0800

    first step towards custom search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,57 @@
 'use client'
-import {SwaggerUIBundle, SwaggerUIStandalonePreset} from 'swagger-ui-dist'
+import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist'
 import 'swagger-ui-dist/swagger-ui.css';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import urls from '../public/urls.json'
+
+const CustomPreset = [
+  SwaggerUIBundle.plugins.TopbarPlugin
+]
+
 export default function Home() {
 
   return (
     <>
-    <a href='https://github.com/Amaizzzzz/azure-rest-api-swagger'>GitHub</a>
-    <div id="swagger"></div>
-    <SwaggerComponent />
+      <a href='https://github.com/Amaizzzzz/azure-rest-api-swagger'>GitHub</a>
+      <SwaggerComponent />
     </>
   )
 }
 
 function SwaggerComponent() {
+  const [swaggerObject, setSwaggerObject] = useState(null);
+  const ref = useRef(null);
   useEffect(() => {
-    SwaggerUIBundle({
-      dom_id: '#swagger',
-      urls: urls,
-      presets: [
-        SwaggerUIBundle.presets.apis,
-        SwaggerUIStandalonePreset
-      ],
-      layout: "StandaloneLayout"
-    })
-  },[])
-  return (<></>)
+    if (ref.current != null) {
+      const swaggerObject = SwaggerUIBundle({
+        // dom_id: '#swagger',
+        domNode: ref.current,
+        url: "https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2022-08-01/apimapis.json",
+        // urls: urls,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          // SwaggerUIStandalonePreset,
+          // CustomPreset
+        ],
+        // layout: "StandaloneLayout"
+      })
+      setSwaggerObject(swaggerObject);
+    }
+  }, [setSwaggerObject, ref]);
+
+  return (<>
+    <button onClick={() => {
+      if (swaggerObject != null) {
+        swaggerObject.specActions.updateUrl("https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2022-08-01/apimapis.json")
+        swaggerObject.specActions.download("https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2022-08-01/apimapis.json")
+      }
+    }}>API 1</button>
+    <button onClick={() => {
+      if (swaggerObject != null) {
+        swaggerObject.specActions.updateUrl("https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2022-08-01/apimdeployment.json")
+        swaggerObject.specActions.download("https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2022-08-01/apimdeployment.json")
+      }
+    }}>API 2</button>
+    <div ref={ref} />
+  </>);
 }
