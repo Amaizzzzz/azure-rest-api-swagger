6b5a98a2e3edf5a274a8339cd6aba3d8ffd33866
Zheyu Shen <zheyushen@microsoft.com>
2023-08-11 16:08:22 +0800

    better filtering

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 'use client';
 import SwaggerUI from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import urls from '../public/urls.json'
-import Select from 'react-select'
+import Select, { createFilter } from 'react-select'
+import { FilterOptionOption } from 'react-select/dist/declarations/src/filters';
+
+// infer the type of urls
+type Urls = typeof urls;
+
+// infer the type of elements of urls
+type Url = Urls[number];
 
 export default function Home() {
 
@@ -21,23 +28,35 @@ function SwaggerComponent() {
     if (ref.current != null) {
       const swaggerObject = SwaggerUI({
         domNode: ref.current,
-        url: "https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/apimanagement/resource-manager/Microsoft.ApiManagement/stable/2022-08-01/apimapis.json",
-        // urls: urls,
       })
       setSwaggerObject(swaggerObject);
     }
   }, [setSwaggerObject, ref]);
 
+  // TODO: refine filter to support acronyms
+  const filterOption = useMemo(() => createFilter({
+    ignoreCase: true,
+    ignoreAccents: true,
+    trim: true,
+    matchFrom: "any" as const,
+    stringify: (option: FilterOptionOption<Url>) => `${option.data.name} ${option.data.url}`,
+  }), []);
+
+  // TODO: better styling
   return (<>
     <a href='https://github.com/Amaizzzzz/azure-rest-api-swagger'>GitHub</a>
     {(swaggerObject != null) ? (<>
       <Select
+        // TODO: add hint for what to type
         options={urls}
         getOptionLabel={(option) => option.name}
         onChange={(e) => {
           swaggerObject.specActions.updateUrl(e.url)
           swaggerObject.specActions.download(e.url)
-        }} />
+        }
+        }
+        filterOption={filterOption}
+      />
     </>) : (<></>)}
     <div ref={ref} />
   </>);
