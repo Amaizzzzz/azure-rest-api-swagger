5eb6ffbc2e2f91430f84835c4ceb0d57ea0eb58e
hoppe <hoppewang@microsoft.com>
2023-09-12 17:07:59 +0800

    fix ui issue and adjust style

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,30 @@
 import SwaggerUI from "swagger-ui";
 import "swagger-ui/dist/swagger-ui.css";
 import { useEffect, useMemo, useRef, useState } from "react";
-import urls from "../public/urls.json";
+import { apiOptions, IApiOption, IApiVersionOption } from "./transform-data";
 import Select, { createFilter } from "react-select";
 import { FilterOptionOption } from "react-select/dist/declarations/src/filters";
+import { findApi, findApiVersion } from "./utils";
+import css from "./page.module.css";
+import "./globals.css";
 
-type UrlGroups = typeof urls;
+export default function Home({ searchParams }) {
+  const api = searchParams.api && decodeURIComponent(searchParams.api);
+  const version =
+    searchParams.version && decodeURIComponent(searchParams.version);
 
-type UrlGroup = UrlGroups[number];
-
-type UrlOptions = UrlGroup["api_version_options"];
-
-type UrlOption = UrlGroup["api_version_options"][number];
-
-export default function Home() {
   return (
     <>
-      <SwaggerComponent />
+      <header className={css.header}>
+        <h1>Azure REST API Swagger</h1>
+        <a
+          href="https://github.com/Amaizzzzz/azure-rest-api-swagger"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </header>
+      <SwaggerComponent api={api} version={version} />
     </>
   );
 }
@@ -26,17 +34,29 @@ function makeAcronym(s: string) {
   return s.replace(/[^A-Z]/g, "");
 }
 
-function SwaggerComponent() {
+function SwaggerComponent(props: { api?: string; version?: string }) {
+  const { api, version } = props;
   const [swaggerObject, setSwaggerObject] = useState(null);
   const ref = useRef(null);
 
-  const [applicableAPIVersionOptions, setApplicableAPIVersionOptions] =
-    useState<UrlOptions>([]);
+  const [selectedApi, setSelectedApi] = useState<IApiOption>(findApi(api));
 
-  const [defaultAPIVersionOption, setDefaultAPIVersionOption] =
-    useState<UrlOption | null>(null);
+  const [selectedVersion, setSelectedVersion] = useState<IApiVersionOption>(
+    findApiVersion(selectedApi, version)
+  );
 
-  const [currentUrl, setCurrentUrl] = useState<string | null>(null);
+  const swaggerUrl = selectedVersion?.value;
+
+  const handleSelectApi = (e: IApiOption) => {
+    if (e !== selectedApi) {
+      setSelectedApi(e);
+      setSelectedVersion(e.value.versions?.[0]);
+    }
+  };
+
+  const handleSelectVersion = (e: IApiVersionOption) => {
+    setSelectedVersion(e);
+  };
 
   useEffect(() => {
     if (ref.current != null) {
@@ -45,21 +65,29 @@ function SwaggerComponent() {
       });
       setSwaggerObject(swaggerObject);
     }
-  }, [setSwaggerObject, ref]);
+  }, []);
 
   useEffect(() => {
-    if (applicableAPIVersionOptions.length > 0) {
-      setDefaultAPIVersionOption(applicableAPIVersionOptions[0]);
-      setCurrentUrl(applicableAPIVersionOptions[0].url);
+    // update url query param
+    const url = new URL(window.location.href);
+    if (selectedApi) {
+      url.searchParams.set("api", encodeURIComponent(selectedApi.label));
     }
-  }, [applicableAPIVersionOptions]);
+    if (selectedVersion) {
+      url.searchParams.set(
+        "version",
+        encodeURIComponent(selectedVersion.label)
+      );
+    }
+    window.history.pushState({}, "", url.toString());
+  }, [selectedApi, selectedVersion]);
 
   useEffect(() => {
-    if (swaggerObject != null && currentUrl != null) {
-      swaggerObject.specActions.updateUrl(currentUrl);
-      swaggerObject.specActions.download(currentUrl);
+    if (swaggerObject != null && swaggerUrl != null) {
+      swaggerObject.specActions.updateUrl(swaggerUrl);
+      swaggerObject.specActions.download(swaggerUrl);
     }
-  }, [swaggerObject, currentUrl]);
+  }, [swaggerObject, swaggerUrl]);
 
   // TODO: refine filter to support acronyms
   const filterOption = useMemo(
@@ -69,8 +97,10 @@ function SwaggerComponent() {
         ignoreAccents: true,
         trim: true,
         matchFrom: "any" as const,
-        stringify: (option: FilterOptionOption<UrlGroup>) =>
-          `${option.data.label} ${option.data.path_without_api_version} ${makeAcronym(option.data.label)}`,
+        stringify: (option: FilterOptionOption<IApiOption>) =>
+          `${option.data.label} ${option.data.value.path} ${makeAcronym(
+            option.data.label
+          )}`,
       }),
     []
   );
@@ -78,40 +108,25 @@ function SwaggerComponent() {
   // TODO: better styling
   return (
     <>
-      <a href="https://github.com/Amaizzzzz/azure-rest-api-swagger">GitHub</a>
-      {swaggerObject != null ? (
-        <>
-          <Select<UrlGroup>
-            options={urls}
-            onChange={(e) => {
-              setApplicableAPIVersionOptions(e.api_version_options);
-            }}
-            placeholder="Select a service..."
-            filterOption={filterOption}
-          />
-          {applicableAPIVersionOptions.length != 0 ? (
-            <Select<UrlOption>
-              // TODO: add hint for what to type
-              options={applicableAPIVersionOptions}
-              defaultValue={defaultAPIVersionOption}
-              // force rerender when the list of applicable API version changes, so that the default API version is selected
-              key={defaultAPIVersionOption?.url}
-              getOptionLabel={(x) => `${x.api_version}`}
-              onChange={(e) => {
-                if (e != null) {
-                  setCurrentUrl(e.url);
-                }
-              }}
-              placeholder="Select an API version..."
-            />
-          ) : (
-            <></>
-          )}
-        </>
-      ) : (
-        <></>
+      <Select<IApiOption>
+        className={css.select}
+        value={selectedApi}
+        options={apiOptions}
+        onChange={handleSelectApi}
+        placeholder="Select a service..."
+        filterOption={filterOption}
+      />
+      {selectedApi?.value?.versions?.length && (
+        <Select<IApiVersionOption>
+          className={css.select}
+          value={selectedVersion}
+          // TODO: add hint for what to type
+          options={selectedApi?.value?.versions}
+          onChange={handleSelectVersion}
+          placeholder="Select an API version..."
+        />
       )}
-      <div ref={ref} />
+      <div ref={ref}/>
     </>
   );
 }
