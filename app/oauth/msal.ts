e29d9c05cdbec44803355c700c81a60f41d6ec27
hoppe <hoppewang@microsoft.com>
2023-09-14 14:14:20 +0800

    change redirect url2

--- app/oauth/msal.ts
+++ app/oauth/msal.ts
@@ -3,11 +3,12 @@ import { PublicClientApplication, Configuration } from "@azure/msal-browser";
 const CLIENT_ID = "f0eaf1fc-c137-4bd1-8a54-f305273e64f4";
 const TENANT_ID = "359cb345-7b01-4eb5-b890-f2f08666a7c1";
 
+const IN_BROWER = typeof window !== "undefined";
 const msalConfig: Configuration = {
     auth: {
         clientId: CLIENT_ID,
         authority: "https://login.microsoftonline.com/" + TENANT_ID,
-        redirectUri: window.location.origin,
+        redirectUri: IN_BROWER ? window.location.origin : undefined,
     },
     cache: {
         cacheLocation: "sessionStorage",
@@ -21,4 +22,4 @@ export const tokenRequest = {
 
 
 export const msal = new PublicClientApplication(msalConfig);
-msal.initialize();
+IN_BROWER && msal.initialize();
