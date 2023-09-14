3906268febafc4702c774f48938deafa67b1a4fe
hoppe <hoppewang@microsoft.com>
2023-09-14 14:37:20 +0800

    change redirect url3

--- app/oauth/msal.ts
+++ app/oauth/msal.ts
@@ -4,11 +4,14 @@ const CLIENT_ID = "f0eaf1fc-c137-4bd1-8a54-f305273e64f4";
 const TENANT_ID = "359cb345-7b01-4eb5-b890-f2f08666a7c1";
 
 const IN_BROWER = typeof window !== "undefined";
+
+const IS_DEVELOP = process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
+
 const msalConfig: Configuration = {
     auth: {
         clientId: CLIENT_ID,
         authority: "https://login.microsoftonline.com/" + TENANT_ID,
-        redirectUri: IN_BROWER ? window.location.origin : undefined,
+        redirectUri: IS_DEVELOP ? "http://localhost:3000" : process.env.NEXT_PUBLIC_VERCEL_URL,
     },
     cache: {
         cacheLocation: "sessionStorage",
