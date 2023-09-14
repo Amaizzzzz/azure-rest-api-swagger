a0c132ec2ac23f6f34453d98f23a89a0f5b34ff7
hoppe <hoppewang@microsoft.com>
2023-09-14 15:02:35 +0800

    use env var5

--- app/oauth/msal.ts
+++ app/oauth/msal.ts
@@ -5,21 +5,19 @@ const TENANT_ID = "359cb345-7b01-4eb5-b890-f2f08666a7c1";
 
 const IN_BROWER = typeof window !== "undefined";
 const msalConfig: Configuration = {
-    auth: {
-        clientId: CLIENT_ID,
-        authority: "https://login.microsoftonline.com/" + TENANT_ID,
-        redirectUri: IN_BROWER ? window.location.origin : undefined,
-    },
-    cache: {
-        cacheLocation: "sessionStorage",
-    },
+  auth: {
+    clientId: CLIENT_ID,
+    authority: "https://login.microsoftonline.com/" + TENANT_ID,
+    redirectUri: IN_BROWER ? window.location.origin : undefined,
+  },
+  cache: {
+    cacheLocation: "sessionStorage",
+  },
 };
-
 
 export const tokenRequest = {
-    scopes: ['https://management.azure.com/user_impersonation']
+  scopes: ["https://management.azure.com/user_impersonation"],
 };
-
 
 export const msal = new PublicClientApplication(msalConfig);
 IN_BROWER && msal.initialize();
