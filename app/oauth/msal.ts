519b66dac61dae3692afc654ceb70c09bac262a8
hoppe <hoppewang@microsoft.com>
2023-09-14 14:38:42 +0800

    use env var

--- app/oauth/msal.ts
+++ app/oauth/msal.ts
@@ -4,7 +4,6 @@ const CLIENT_ID = "f0eaf1fc-c137-4bd1-8a54-f305273e64f4";
 const TENANT_ID = "359cb345-7b01-4eb5-b890-f2f08666a7c1";
 
 const IN_BROWER = typeof window !== "undefined";
-
 const IS_DEVELOP = process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
 
 const msalConfig: Configuration = {
